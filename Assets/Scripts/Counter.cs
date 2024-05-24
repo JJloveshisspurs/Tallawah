d67ee15d40a7245bbe908fb790f57a384dc6b0ef
amangreen123 <Aaron.green1@marist.edu>
2024-05-24 13:14:34 -0400

    Adjusted the code and also added a score limit

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -2,69 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System;
-using UnityEngine.SocialPlatforms.Impl;
-
 
 public class Counter : MonoBehaviour
 {
-
     public static Counter instance;
     public TMP_Text coinText;
     public int currentCoins = 0;
-    bool rastaManActive = false;
+    public int maxCoins = 7;
+    private bool rastaManActive = false;
 
     private void Awake()
     {
-        instance = this;
+        // Ensure that only one instance of Counter exists.
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
     }
-    
 
     // Start is called before the first frame update
     void Start()
     {
-        coinText.text = "COINS: " + currentCoins.ToString();
+        UpdateCoinText();
     }
 
     void Update()
     {
-        if (currentCoins >= 7 && rastaManActive == false)
+        if (currentCoins >= maxCoins && !rastaManActive)
         {
-            rastaManAppears();
+            RastaManAppears();
         }
-        
     }
 
     public void IncreaseCoins(int amount)
     {
         currentCoins += amount;
-
         if (currentCoins < 0)
         {
             currentCoins = 0;
         }
-
-        coinText.text =  "COINS:" + currentCoins.ToString();
-
+        UpdateCoinText();
     }
 
     public void DecreaseCoins(int amount)
     {
         currentCoins -= amount;
-
-        if(currentCoins < 0)
+        if (currentCoins < 0)
         {
             currentCoins = 0;
         }
-
-        coinText.text = "COINS: " + currentCoins.ToString();
+        UpdateCoinText();
     }
 
-    public void rastaManAppears ()
+    private void UpdateCoinText()
+    {
+        if (coinText != null)
+        {
+            coinText.text = $"COINS: {currentCoins} / {maxCoins}";
+        }
+    }
+
+    private void RastaManAppears()
     {
         rastaManActive = true;
         Debug.Log("RastaMan Activated");
+        // Add your logic here for RastaMan's appearance.
     }
-
 }
-
