65695e9214b2a1001a7531daa727b4877669c494
JonathanJenningsMindshow <jjennings0309@gmail.com>
2024-05-24 15:57:11 -0700

    Narrative Lerp Behaviors
    Finished creating lerp behaviors of character sprites during narration

---
+++ Assets/Scripts/Narrative/NarrativeController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NarrativeController : MonoBehaviour
+{
+
+    public string sceneToLoadOnNarrativeComplete;
+
+    public List<GameObject> narrativePages;
+
+    public int NarrativePanelIndex = -1;
+
+    public float delayBetweenPanels = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DisplayNextPanel());
+    }
+
+    IEnumerator DisplayNextPanel()
+    {
+
+        NarrativePanelIndex = NarrativePanelIndex + 1;
+
+
+
+
+
+        yield return new WaitForSeconds(delayBetweenPanels);
+
+
+
+        if(NarrativePanelIndex < narrativePages.Count)
+        {
+
+            ClearandRenderCorrectNarrativePanel();
+
+        }
+        else
+        {
+
+            Debug.Log("Completed Narrative Panels! Loading next scene");
+
+
+
+            LoadNextScene();
+        }
+        
+
+        
+    }
+
+
+    public void ClearandRenderCorrectNarrativePanel()
+    {
+        for(int i = 0; i < narrativePages.Count; i++)
+        {
+
+            narrativePages[i].SetActive(false);
+
+        }
+
+
+        narrativePages[NarrativePanelIndex].SetActive(true);
+
+
+        StartCoroutine(DisplayNextPanel());
+    }
+
+    public void LoadNextScene()
+    {
+
+        StopAllCoroutines();
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoadOnNarrativeComplete);
+
+    }
+}
---
+++ Assets/Scripts/Narrative/NarrativeFadeItem.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NarrativeFadeItem 
+{
+    public SpriteRenderer sprite1;
+    public float sprite1_Fadetime;
+    public float sprite1_Fadetimer;
+
+
+    public SpriteRenderer sprite2;
+    public float sprite2_Fadetime;
+    public float sprite2_Fadetimer;
+
+}
