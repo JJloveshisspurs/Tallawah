9ae1518544cf4cd06d9d4a2dd0a0f5aea9860377
JonathanJenningsMindshow <jjennings0309@gmail.com>
2024-05-28 12:12:43 -0700

    Updated Seashell movement sprites
    updated sprites for Seashell movement

--- Assets/Scripts/SpriteAnimator.cs
+++ Assets/Scripts/SpriteAnimator.cs
@@ -13,13 +13,19 @@ public class SpriteAnimator : MonoBehaviour
 
     public int currentFrameIndex = 0;
 
+    public bool playOnStart = true;
+
+    bool playingAnimation = false;
 
     public void Start()
     {
-        currentFrameIndex = 0;
-        currentSprite.sprite = images[currentFrameIndex];
+        if (playOnStart)
+        {
+            currentFrameIndex = 0;
+            currentSprite.sprite = images[currentFrameIndex];
 
-        StartCoroutine(Animate());
+            StartCoroutine(Animate());
+        }
     }
 
 
@@ -35,5 +41,30 @@ public class SpriteAnimator : MonoBehaviour
         yield return new WaitForSeconds(animationSpeed);
 
         StartCoroutine(Animate());
+    }
+
+    public void StartAnimating()
+    {
+        if (playingAnimation == false)
+        {
+            playingAnimation = true;
+            StopAllCoroutines();
+            currentFrameIndex = 0;
+            currentSprite.sprite = images[currentFrameIndex];
+
+            StartCoroutine(Animate());
+        }
+
+    }
+
+    public void StopAnimating()
+    {
+        if (playingAnimation)
+        {
+
+            playingAnimation = false;
+            StopAllCoroutines();
+        }
+
     }
 }
