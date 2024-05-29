209566c5b1f08434a9d37f6e2bc9426342d128ae
Pkprempeh <pkprempeh@gmail.com>
2024-05-28 19:27:31 -0500

    Add files via upload
    Animation for the chickens

---
+++ Assets/AiEnemy/Scripts/SimpleChickenAnimation.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class SimpleSpriteAnimation : MonoBehaviour
+{
+    public SpriteRenderer currentSprite;
+
+    public List<Sprite> images;
+
+    public float animationSpeed = .25f;
+
+
+    public int currentFrameIndex = 0;
+    
+
+    public void Start()
+    {
+        currentFrameIndex = 0;
+        currentSprite.sprite = images[currentFrameIndex];
+
+        StartCoroutine(Animate());
+    }
+
+
+    IEnumerator Animate()
+    {
+        currentFrameIndex = currentFrameIndex + 1;
+
+        if (currentFrameIndex >= images.Count)
+            currentFrameIndex = 0;
+
+        currentSprite.sprite = images[currentFrameIndex];
+
+        yield return new WaitForSeconds(animationSpeed);
+
+        StartCoroutine(Animate());
+    }
+}
